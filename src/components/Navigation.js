b1df4a1d042988315c312b3a3a632c5f18d3577e
hamishmcgregor3 <hmcgregor@wustl.edu>
2024-11-23 20:58:01 +1100

    inital commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+export default function Navigation() {
+    return (
+        <nav>
+            <div>
+                <a href="/">Home</a>
+            </div>
+            <div>
+                <a href="/contact">Contact</a>
+            </div>
+        </nav>
+    );
+}
